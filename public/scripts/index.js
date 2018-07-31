dbba750a9b380e5a4662daf0675694cc87bdae33
Ethan Titler <Titler.ethan.chs@gmail.com>
2018-07-31 14:22:17 -0500

    added Environment urls

---
+++ public/scripts/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let app = app || {};
+
+(function(module){
+
+  let productionUrl = 'https://paranormal-map-tivity.herokuapp.com';
+  let developmentUrl = 'http://localhost:3000';
+
+  module.inProduction = /^(?!localhost|127)/.test(window.location.hostname);
+
+  module.Environment = module.inProduction ? developmentUrl : productionUrl;
+
+})(app);

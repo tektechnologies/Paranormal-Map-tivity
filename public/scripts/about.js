78d3a80ea6851fabf020ea6449efa5d48ac4002e
Ethan Titler <Titler.ethan.chs@gmail.com>
2018-08-02 14:54:00 -0500

    added semi-colons

--- public/scripts/about.js
+++ public/scripts/about.js
@@ -4,19 +4,19 @@ var app = app || {};
 
 (function(module){
 
-  var pages = {}
+  var pages = {};
   module.pages = pages;
 
   pages.initMap = () => {
     console.log('loading');
     app.showOnly('.map-view');
     $('.dropdown').show();
-  }
+  };
 
   pages.initAboutPage = (event) => {
     $('.about').hide();
     app.showOnly('.about');
-  }
+  };
 
   
 })(app);

878dd36c1957cfb25626d8d45dcc466ea8a915f0
Ethan Titler <Titler.ethan.chs@gmail.com>
2018-08-02 13:54:39 -0500

    moved the detail route

--- public/scripts/routes.js
+++ public/scripts/routes.js
@@ -1,9 +1,10 @@
 'use strict';
 
+
+page('/', () => app.pages.initMap());
+page('/about', ctx => app.pages.initAboutPage(ctx));
 page('/detail/:type/:index', ctx => {
   app.sightings.loadOne(ctx.params.type, parseInt(ctx.params.index));
 });
-page('/', () => app.pages.initMap());
-page('/about', ctx => app.pages.initAboutPage(ctx));
 
 page();

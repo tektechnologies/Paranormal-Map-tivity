3f2a88c21efdf49948db9ffe4c841288c2c245cf
Ben Van Meter <vanmeterx@gmail.com>
2018-08-01 19:10:30 -0500

    removed test logs

--- public/scripts/routes.js
+++ public/scripts/routes.js
@@ -2,10 +2,6 @@
 
 page('/', () => app.pages.initMapPage());
 page('/detail/:type/:index', ctx => {
-  console.log(ctx);
-  console.log(ctx.params.type);
-  console.log(ctx.params.index);
-  console.log(app.sightings.loadOne);
   app.sightings.loadOne(ctx.params.type, parseInt(ctx.params.index));
 });
 page('/about', ctx => app.pages.initAboutPage(ctx));

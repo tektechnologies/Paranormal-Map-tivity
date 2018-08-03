650665d05be4c16ec0d3d27e32583a48bb704c2d
Ethan Titler <Titler.ethan.chs@gmail.com>
2018-08-03 10:35:29 -0500

    removed unused code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -18,21 +18,15 @@ var app = app || {};
 
     $.get(`${app.Environment}/api/${type}/${index}`)
       .then(newData => {
-        let one={};
         if(newData.description){
-          one = new app.GhostSighting(newData);
           $('.detail-view').empty().append(new app.GhostSighting(newData).toHtml());
         }
         if(newData.text){
-          one = new app.UfoSighting(newData);
           $('.detail-view').empty().append(new app.UfoSighting(newData).toHtml());
         }
         if(newData.observed){
-          console.log(newData);
-          console.log(new app.BigFootSighting(newData));
           $('.detail-view').empty().append(new app.BigFootSighting(newData).toHtml());
         }
-        console.log(one);
       });
   };
 
